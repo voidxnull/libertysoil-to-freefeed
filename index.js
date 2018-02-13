b2a36077e9148a759e38165af06dc2a70ed6622c
Dmitry Vdovin <voidxnull@gmail.com>
2018-02-13 17:23:16 +0200

    [WIP] Import users

--- index.js
+++ index.js
@@ -11,14 +11,19 @@ const { createLsConnection, createFfConnection } = require('./db');
 
 program
   .version('1.0.0')
-  .option('-d, --dump-dir [path]', 'directory containing dump files (users.js, comments.js, etc.)')
-  .option('--ls-conn [str]', 'libertysoil connection string')
-  .option('--ff-conn [str]', 'freefeed connection string');
+  .option('-d, --dump-dir [path]', 'Directory containing dump files (users.js, comments.js, etc.)', process.cwd())
+  .option('--ls-conn [str]', 'Libertysoil connection string')
+  .option('--ff-conn [str]', 'Freefeed connection string');
 
 program
   .command('dump-ls-db')
-  .description('dumps libertysoil tables to json files')
+  .description('Dumps libertysoil tables to json files')
   .action(asyncAction(dumpLsTables, 'Dumping LibertySoil database'));
+
+program
+  .command('import [tables...]')
+  .description('Import specified freefeed tables from libertysoil dumps.')
+  .action(asyncAction(importCommand))
 
 program.parse(process.argv);
 
@@ -26,7 +31,7 @@ if (!process.argv.slice(2).length) {
   program.help();
 }
 
-function asyncAction(command, title) {
+function asyncAction(command, title = null) {
   return async (...args) => {
     const spinner = ora(title).start();
     try {
@@ -38,19 +43,6 @@ function asyncAction(command, title) {
       process.exit(1);
     }
   }
-}
-
-async function dumpLsTables() {
-  const client = await createLsConnection(program);
-  const dumpPath = path.resolve(program.dumpDir || process.cwd());
-
-  await dumpTable(client, 'users', dumpPath);
-  await dumpTable(client, 'posts', dumpPath);
-  await dumpTable(client, 'comments', dumpPath);
-  await dumpTable(client, 'followers', dumpPath);
-  await dumpTable(client, 'likes', dumpPath);
-
-  client.end();
 }
 
 async function forEachRowChunked(client, table, callback, options = { batch: 1000 }) {
@@ -93,4 +85,44 @@ async function dumpTable(client, table, outDir) {
       }
     );
   });
+}
+
+async function dumpLsTables() {
+  const client = await createLsConnection(program);
+  const dumpPath = path.resolve(program.dumpDir);
+
+  await dumpTable(client, 'users', dumpPath);
+  await dumpTable(client, 'posts', dumpPath);
+  await dumpTable(client, 'comments', dumpPath);
+  await dumpTable(client, 'followers', dumpPath);
+  await dumpTable(client, 'likes', dumpPath);
+
+  client.end();
+}
+
+const ALL_FREEFEED_TABLES = ['users', 'posts', 'comments', 'subscriptions', 'likes'];
+
+async function importCommand(tables) {
+  if (tables.length === 0) {
+    tables = ALL_FREEFEED_TABLES;
+  }
+
+  for (const table of tables) {
+    switch (table) {
+      case 'users': await importUsers(); break;
+      case 'posts': break;
+      case 'comments': break;
+      case 'subscriptions': break;
+      case 'likes': break;
+      default: throw Error(`Unknown table: '${table}'`);
+    }
+  }
+}
+
+async function importUsers() {
+  const client = await createFfConnection(program);
+  const usersPath = path.resolve(path.join(program.dumpDir, 'users.json'));
+  const fileStream = fs.createReadStream(usersPath);
+  const stream = fileStream.pipe(JSONStream.parse());
+
 }
